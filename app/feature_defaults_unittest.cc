2a817c26676eaa5139383962aa9dd70040d75356
Claudio DeSouza <cdesouza@brave.com>
2025-05-21 11:59:26 +0100

    [cr138] `kEnabledStructuredMetricsService` deleted
    This flag is gone, however the code it was guarding is still under
    `metrics::features::kStructuredMetrics`.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/a3137e268052078d7c835105054cb10299a381ea
    
    commit a3137e268052078d7c835105054cb10299a381ea
    Author: Xiaochen Zhou <xiaochenzh@chromium.org>
    Date:   Tue May 20 08:10:11 2025 -0700
    
        Clean up stale base::Feature "EnableStructuredMetricsService"
    
        Also changed related feature params to constants. Removed feature
        disabled test. Move the constants into corresponding cc files since
        they all have only one callsite.
    
        Bug: 356236109
        Change-Id: I977fd4a93b77a6f3041b211589bc910e702182c5
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6547775
        Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
        Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1462802}

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -192,7 +192,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
       &lens::features::kLensStandalone,
       &manta::features::kMantaService,
       &media::kLiveCaption,
-      &metrics::structured::kEnabledStructuredMetricsService,
       &metrics::structured::kPhoneHubStructuredMetrics,
       &net::features::kEnableWebTransportDraft07,
       &net::features::kTopLevelTpcdOriginTrial,
