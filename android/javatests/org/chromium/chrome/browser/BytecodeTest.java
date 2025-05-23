69b861e5880d329d47c02c8daa3be650c12bf7e0
Serg <serg.zhukovsky@gmail.com>
2025-05-23 11:36:26 -0400

    Uses a correct profile for UrlSanitizerService on Android

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -745,6 +745,12 @@ public class BytecodeTest {
                         int.class));
         Assert.assertTrue(
                 methodExists(
+                        "org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulator",
+                        "getProfile",
+                        MethodModifier.REGULAR,
+                        Profile.class));
+        Assert.assertTrue(
+                methodExists(
                         "org/chromium/base/shared_preferences/StrictPreferenceKeyChecker",
                         "isKeyInUse",
                         MethodModifier.REGULAR,
@@ -2320,6 +2326,14 @@ public class BytecodeTest {
                 fieldExists(
                         "org/chromium/chrome/browser/customtabs/CustomTabActivity",
                         "mIsEnterAnimationCompleted"));
+        Assert.assertTrue(
+                fieldExists(
+                        "org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulator",
+                        "mItemDelegate"));
+        Assert.assertTrue(
+                fieldExists(
+                        "org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulator",
+                        "mParams"));
     }
 
     @Test
