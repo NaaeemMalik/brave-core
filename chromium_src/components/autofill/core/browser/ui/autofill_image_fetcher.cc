3bcd6ec594216464ddc344cce8fd6dc63d23d93f
Max Karolinskiy <max@brave.com>
2025-05-23 13:32:04 -0400

    Disables autofill image fetcher.
    Per privacy/security. Autofill image fetcher get images from a Google
    server.

---
+++ chromium_src/components/autofill/core/browser/ui/autofill_image_fetcher.cc
@@ -0,0 +1,43 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "components/autofill/core/browser/ui/autofill_image_fetcher.h"
+
+#include "base/notreached.h"
+#include "ui/gfx/image/image.h"
+
+namespace autofill {
+
+AutofillImageFetcher::~AutofillImageFetcher() = default;
+
+void AutofillImageFetcher::FetchCreditCardArtImagesForURLs(
+    base::span<const GURL> image_urls,
+    base::span<const AutofillImageFetcherBase::ImageSize> image_sizes_unused) {}
+
+// Only implemented in Android clients. Pay with Pix is only available in
+// Chrome on Android.
+void AutofillImageFetcher::FetchPixAccountImagesForURLs(
+    base::span<const GURL> image_urls) {
+  NOTREACHED();
+}
+
+void AutofillImageFetcher::FetchValuableImagesForURLs(
+    base::span<const GURL> image_urls) {}
+
+const gfx::Image* AutofillImageFetcher::GetCachedImageForUrl(
+    const GURL& image_url,
+    ImageType image_type) const {
+  return nullptr;
+}
+
+gfx::Image AutofillImageFetcher::ResolveCardArtImage(
+    const GURL& card_art_url,
+    const gfx::Image& card_art_image) {
+  return card_art_image;
+}
+
+AutofillImageFetcher::AutofillImageFetcher() = default;
+
+}  // namespace autofill
